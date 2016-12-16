a7b51ddbb12710383f819e16830c2fa574c710dd
thinkhappily <thinkhappily@gmail.com>
2016-12-16 19:05:04 +0800

    Moved AngularJS project as main directory. Updated .gitignore file Added config to not check freemarker template location.

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -22,7 +22,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/es5-shim/es5-shim.js',
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
@@ -36,7 +35,7 @@ module.exports = function(config) {
       'bower_components/ng-table/dist/ng-table.min.js',
       'bower_components/moment/moment.js',
       'bower_components/angular-growl-v2/build/angular-growl.js',
-      'bower_components/angular-file-upload/angular-file-upload.js',
+      'bower_components/angular-file-upload/angular-file-upload.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
